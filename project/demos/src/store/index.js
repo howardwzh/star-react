c5e0eb3ade5ee66e06c46fb43ea25aded67454a3
王忠华 <wangzhonghua@ppmoney.com>
2018-05-21 18:41:59 +0800

    update rematch demo

--- project/demos/src/store/index.js
+++ project/demos/src/store/index.js
@@ -1,8 +1,12 @@
 import { init } from '@rematch/core'
+import { createLogger } from 'redux-logger'
 import * as models from './models'
 
 const store = init({
-  models
+  models,
+  redux: {
+    middlewares: [createLogger()]
+  }
 })
 
 export default store
